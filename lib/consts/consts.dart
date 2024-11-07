48298350d7d44aab1d4a8ba3c2b9383defebdb9f
Abubakr Memon <memabubakr@gmail.com>
2024-11-07 21:28:05 +0530

    Add all project files

---
+++ lib/consts/consts.dart
@@ -0,0 +1,18 @@
+export './colors.dart';
+// ignore: duplicate_export
+export './colors.dart';
+export './images.dart';
+export './strings.dart';
+export './styles.dart';
+// ignore: depend_on_referenced_packages
+export 'package:velocity_x/velocity_x.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
